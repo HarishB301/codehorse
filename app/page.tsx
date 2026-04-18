390b9dc168567593f3f40a5c90246f0de5421c0a
HarishB301 <harish33301@gmail.com>
2026-04-18 09:55:08 +0530

    04_dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
-import React from 'react'
-import { Button } from '@/components/ui/button'
+
 import { requireAuth } from '@/module/auth/utils/auth-utils'
-import Logout from '@/module/auth/components/logout'
+import { redirect } from 'next/navigation'
+
+
 const Home=async() => {
   await requireAuth()
-  return (
-
-    <div className='flex flex-col items-center justify-center h-screen'> 
-      <Logout >
-        <Button>Logout</Button>
-        {/* <h2>hello</h2> */}
-      </Logout>
-    </div>
-  )
+  return redirect('/dashboard')
 }
 
 export default Home
